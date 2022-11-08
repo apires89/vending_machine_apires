7e7f2367be3003df47dc7b9d2b818c4bcbbd8d37
andre <andrecabralpires@gmail.com>
2022-11-08 15:45:22 +0000

    testing api with login

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module VendingMachineApires
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -37,3 +40,5 @@ module VendingMachineApires
     config.api_only = true
   end
 end
+
+curl -XGET -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwic2NwIjoidXNlciIsImF1ZCI6bnVsbCwiaWF0IjoxNjY3OTIyMTA0LCJleHAiOjE2Njc5MjU3MDQsImp0aSI6IjhiOWU4OTYzLWFhZjAtNGMzZS1hNTc3LTA4MTYwZGU5MDUxZiJ9.dUGL1OjVylaqsv7LCnEJs8vZR465oEG0095PBhJ8pug" -H "Content-Type: application/json" http://localhost:3000/member-data
