05229f762011fe5d7b282dc4c9b0447011bbba84
andre <andrecabralpires@gmail.com>
2022-11-08 15:28:01 +0000

    created jwt setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_08_151015) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_151755) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
 
   create_table "products", force: :cascade do |t|
     t.integer "amountAvailable"
