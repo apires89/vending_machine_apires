2908fdf20d559856f8a48bdc6ad8d4283a3a2fa3
andre <andrecabralpires@gmail.com>
2022-11-09 09:18:33 +0000

    product api  barebones tested and working

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,17 +1,34 @@
 class Api::V1::ProductsController < Api::V1::BaseController
   before_action :set_product, only: [ :show, :update, :destroy ]
   def index
-    @products = current_user.products
+    @products = Product.all
   end
 
   def show
   end
 
+  def create
+    @product = current_user.products.new(product_params)
+    if @product.save
+      render json: @product, status: :ok
+    else
+      render json: { data: @product.errors.full_messages, status: "failed" }, status: :unprocessable_entity
+    end
+  end
+
   def update
     if @product.update(product_params)
-      render :show
+      render json: @product, status: :ok
     else
-      render_error
+      render json: { data: @product.errors.full_messages, status: "failed" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      render json: { data: 'product deleted successfully', status: 'sucess' }, status: :ok
+    else
+      render json: { data: 'Something went wrong', status: 'failed' }
     end
   end
 
@@ -28,10 +45,13 @@ class Api::V1::ProductsController < Api::V1::BaseController
 
   private
 
+  def product_params
+    params.require(:product).permit(:productName, :cost, :amountAvailable, :seller_id)
+  end
+
   def set_product
     @product = current_user.products.find(params[:id])
     rescue ActiveRecord::RecordNotFound => error
       render json: error.message, status: :unauthorized
-    authorize @product  # For Pundit
   end
 end
